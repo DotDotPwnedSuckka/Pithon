1c07ca8880ffd5f16418a7baefca86cbba7f9a35
Catsy <lockdead456@gmail.com>
2019-05-08 21:08:35 -0300

    Update and rename chudnovsky.py to pithon.py

--- pithon.py
+++ pithon.py
@@ -1,4 +1,4 @@
-#por arthur sandonato 
+
 from math import sqrt
 from decimal import Decimal, getcontext
 from time import sleep 
@@ -7,10 +7,6 @@ a = 3405449678154416971853498155008000000 * sqrt(640320)
 b = 867407410133324147761288805130794983129
 c = a / b
 print(c)
-sleep(2)
-#sessão do cálculo da circunferência da terra
-t = c * 12742 #usando o diametro
-print(t)
 sleep(1)
-t2 = 2 * c * 6378 #usando o radio
+t2 = 2 * c * 6378 
 print(t2)
